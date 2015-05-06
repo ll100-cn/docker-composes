92dc7a42a9dd156f58421e1dd1b87959abd5d1b0
sunteya <sunteya@gmail.com>
2015-05-06 12:34:08 +0800

    leaf: add sftp role

--- leaf/40_enable_role_service.sh
+++ leaf/40_enable_role_service.sh
@@ -9,3 +9,7 @@ if [[ $ROLE == *"sidekiq"* ]]; then
 	rm -f /etc/service/sidekiq/down
 	rm -f /etc/service/sidekiq-monitor/down
 fi
+
+if [[ $ROLE == *"sftp"* ]]; then
+	[ -n $APP_PASSWORD ] && echo "app:$APP_PASSWORD" | chpasswd
+fi
