0b914ef7039d652d5ef9fed6fbeacd938bc184e3
sunteya <sunteya@gmail.com>
2017-12-06 12:06:08 +0800

    leaf: base on puma instead of passenger

--- leaf/40_enable_role_service.sh
+++ leaf/40_enable_role_service.sh
@@ -2,6 +2,7 @@
 
 if [[ $ROLE == *"passenger"* ]]; then
 	rm -f /etc/service/nginx/down
+	rm -f /etc/service/puma/down
 	rm -f /etc/service/nginx-log-forwarder/down
 	mv /etc/logrotate.d/nginx.disabled /etc/logrotate.d/nginx
 fi
